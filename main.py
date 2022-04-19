e3e632000f1b710042dcb93c5c42c9abfa9891c1
Rishabh Mukund <rishabh.m96@gmail.com>
2022-04-18 21:27:08 -0400

    Fixed bugs

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.name == 'pendulum':
-        img1 = cv2.imread('res/pendulum/im0.png')
-        img2 = cv2.imread('res/pendulum/im1.png')
+        img1 = cv2.imread('res/pendulum/im0.png', 0)
+        img2 = cv2.imread('res/pendulum/im1.png', 0)
         K1 = np.array([[1729.05, 0, -364.24],
                        [0, 1729.05, 552.22],
                        [0, 0, 1]])
@@ -30,8 +30,8 @@ if __name__ == '__main__':
         baseline = 537.75
         f = 1729.05
     elif args.name == 'octagon':
-        img1 = cv2.imread('res/octagon/im0.png')
-        img2 = cv2.imread('res/octagon/im1.png')
+        img1 = cv2.imread('res/octagon/im0.png', 0)
+        img2 = cv2.imread('res/octagon/im1.png', 0)
         K1 = np.array([[1742.11, 0, 804.90],
                        [0, 1742.11, 541.22],
                        [0, 0, 1]])
@@ -41,8 +41,8 @@ if __name__ == '__main__':
         baseline = 221.76
         f = 1742.11
     else:
-        img1 = cv2.imread('res/curule/im0.png')
-        img2 = cv2.imread('res/curule/im1.png')
+        img1 = cv2.imread('res/curule/im0.png', 0)
+        img2 = cv2.imread('res/curule/im1.png', 0)
         K1 = np.array([[1758.23, 0, 977.42],
                        [0, 1758.23, 552.15],
                        [0, 0, 1]])
@@ -58,18 +58,15 @@ if __name__ == '__main__':
     img1 = cv2.resize(img1, (width, height), interpolation=cv2.INTER_AREA)
     img2 = cv2.resize(img2, (width, height), interpolation=cv2.INTER_AREA)
 
-    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-
     while(1):
         try:
             img_keypoints, list_kp1, list_kp2 = utils.featureMapping(
-                gray1, gray2)
+                img1, img2)
             F, error = utils.ransacFMatrix([list_kp1, list_kp2])
             E = utils.getEMatrix(F, K1, K2)
             cam_pose = utils.getCameraPose(E, list_kp1)
-            H1, H2, rect_pts1, rect_pts2, gray1_rect, gray2_rect =\
-                utils.rectification(gray1, gray2, list_kp1, list_kp2, F)
+            H1, H2, rect_pts1, rect_pts2, img1_rect, img2_rect =\
+                utils.rectification(img1, img2, list_kp1, list_kp2, F)
             break
         except Exception:
             continue
@@ -86,16 +83,16 @@ if __name__ == '__main__':
 
     lines1 = cv2.computeCorrespondEpilines(rect_pts2.reshape(-1, 1, 2), 2, F)
     lines1 = lines1.reshape(-1, 3)
-    left_lines, _ = utils.drawlines(gray1_rect, gray2_rect,
+    left_lines, _ = utils.drawlines(img1_rect, img2_rect,
                                     lines1, rect_pts1, rect_pts2)
 
     lines2 = cv2.computeCorrespondEpilines(rect_pts1.reshape(-1, 1, 2), 1, F)
     lines2 = lines2.reshape(-1, 3)
     right_lines, _ = utils.drawlines(
-        gray2_rect, gray2_rect, lines2, rect_pts2, rect_pts1)
+        img2_rect, img1_rect, lines2, rect_pts2, rect_pts1)
 
     disparity_map_unscaled, disparity_map_scaled = utils.ssdCorrespondence(
-        gray1_rect, gray2_rect)
+        img1_rect, img2_rect)
 
     depth_map, depth_array = utils.disparity2Depth(
         baseline, f, disparity_map_unscaled)
